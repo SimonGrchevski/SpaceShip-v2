7161fe126f43ce16fcc3a4871abd86dd3cb1ac52
Simon Grchevski <simongrcevski@gmail.com>
2021-12-18 23:27:26 +0100

    Add position scale rotation and state to the actor class

--- actor.cpp
+++ actor.cpp
@@ -3,6 +3,11 @@
 
 Actor::Actor(Game* game)
   :mGame{game}
+  ,mComponents{}
+  ,mPosition{}
+  ,mScale{0.0}
+  ,mRotation{0,0}
+  ,mState{ Active }
 {
   mGame -> addActor(this);
 }
--- actor.h
+++ actor.h
@@ -1,5 +1,7 @@
 #ifndef ACTOR_18_12_2021_H
 #define ACTOR_18_12_2021_H
+
+#include "vector2d.h"
 #include <vector>
 
 class Game;
@@ -8,6 +10,14 @@ class Component;
 class Actor
 {
   public:
+
+    enum State 
+    {
+      Active,
+      Paused,
+      Dead
+    };
+
     Actor(Game* game);
     ~Actor();
 
@@ -18,6 +28,13 @@ class Actor
   private:
     Game* mGame;
     std::vector<Component*> mComponents;
+
+    Vector2d mPosition;
+    float mScale;
+    float mRotation;
+
+    State mState;
+
 };
 
 #endif

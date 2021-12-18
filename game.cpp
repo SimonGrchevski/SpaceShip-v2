1cf861b8acd4900d0a356db46ae485ef02aaf4f0
Simon Grchevski <simongrcevski@gmail.com>
2021-12-18 22:27:10 +0100

    Implement the initializing of sdl

--- game.cpp
+++ game.cpp
@@ -1,8 +1,10 @@
 #include "game.h"
-
+#include <SDL2/SDL_image.h>
 
 Game::Game()
-  :mActors{}
+  :mWindow{ nullptr }
+  ,mRenderer{ nullptr }
+  ,mActors{}
   ,mPendingActors{}
   ,mIsRuning{true}
   ,mTickCount{0}
@@ -11,6 +13,13 @@ Game::Game()
 
 bool Game::initialize()
 {
+  return (
+    initVideo() && initWindow() && initRenderer() && initImg() && loadData() );
+}
+
+bool Game::loadData()
+{
+  // loading the data
   return true;
 }
 
@@ -18,6 +27,50 @@ void Game::run()
 {
   while(mIsRuning)
   {
-    
+
   }
 }
+
+bool Game::initVideo()
+{
+  if( SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO ) != 0)
+  {
+    SDL_Log("Failed to initialze video %s", SDL_GetError() );
+    return false;
+  }
+  return true;
+}
+
+bool Game::initWindow()
+{
+  mWindow = SDL_CreateWindow("spaceShip", 100,100,1024,768,0);
+  if(!mWindow)
+  {
+    SDL_Log("Failed to initialize the window %s", SDL_GetError() );
+    return false;
+  }
+  return true;
+}
+
+bool Game::initRenderer()
+{
+  mRenderer = SDL_CreateRenderer(mWindow,-1,
+  SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+  if(!mRenderer)
+  {
+    SDL_Log("Failed to initialize renderer %s", SDL_GetError() );
+    return false;
+  }
+  return true;
+}
+
+bool Game::initImg()
+{
+  if( IMG_Init(IMG_INIT_PNG) == 0 )
+  {
+    SDL_Log("Failed to initialize image %s", SDL_GetError() );
+    return false;
+  }
+  return true;
+}
+
--- game.h
+++ game.h
@@ -1,6 +1,7 @@
 #ifndef GAME_18_12_2021_H
 #define GAME_18_12_2021_H
 #include <vector>
+#include <SDL2/SDL.h>
 
 class Actor;
 
@@ -11,11 +12,25 @@ class Game
   bool initialize();
   void run();
 
+  Game(const Game&) = delete;
+  Game(Game&&) = delete;
+  Game& operator=(const Game&) = delete;
+  Game& operator=(Game&&) = delete;
 
   private:
+  SDL_Window* mWindow;
+  SDL_Renderer * mRenderer;
+
   std::vector<Actor*> mActors;
   std::vector<Actor*>mPendingActors;
+
   bool mIsRuning;
-  int mTickCount;
+  uint32_t mTickCount;
+
+  bool loadData();
+  bool initVideo();
+  bool initWindow();
+  bool initRenderer();
+  bool initImg();
 };
 #endif
